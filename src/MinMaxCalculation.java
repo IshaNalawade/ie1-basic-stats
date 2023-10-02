8c8e148e0e620f3656f2959e88915170d0e0c478
Niti Mangwani <nitimangwani@NitiMangwani.local>
2023-10-02 18:36:30 -0400

    Added method for calculating minimum number

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -2,7 +2,9 @@ public class MinMaxCalculation {
     public static void main (String[] args) {
         double [] numarray = { 1.0, 2.0, 3.0, 4.0, 5.0 };
         double maxNo = getMaximumNumber (numarray) ;
+        double minNo = getMinimumNumber (numarray);
         System.out.println ("Maximum value: " + maxNo) ;
+        System.out.println ("Minimum value: " + minNo);
     }
 
     /**
@@ -18,5 +20,21 @@ public class MinMaxCalculation {
             }
         }
         return maxNo;
+    }
+
+
+    /**
+     * Calculates the minimum number from an array
+     * @param numarray is an array of double numbers
+     * @return minimum number
+     */
+    public static double getMinimumNumber (double ... numbers) {
+        double minNo = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minNo) {
+                minNo = numbers[i];
+            }
+        }
+        return minNo;
     }
 }
